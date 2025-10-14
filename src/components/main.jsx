5e9473771858463a765f7e01556162dc74ad1cfd
jvchi <elechidestinyj@gmail.com>
2025-10-14 20:52:16 +0100

    implemented useState in array and delete button

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,26 +1,45 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import {useState} from "react"
 
 export default function Main(){
-  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const [ingredients, setIngredients] = useState(["Chicken", "Oregano", "Tomatoes"]);
 
-  const ingredientListItem = ingredients.map(ingredient=>(
-    <li>{ingredient}</li> 
-  ))
+  const ingredientListItem = ingredients.map((ingredient, index) => (
+    <li key={index}>
+      {ingredient}
+      <FontAwesomeIcon
+        icon={faTrash}
+        className="delete"
+        onClick={() =>
+          setIngredients(prev => prev.filter((_, i) => i !== index))
+        }
+      />
+    </li>
+  ));
+  const [newIngredient, setNewIngredient] = useState("")
 
   function handleSubmit(e){
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const newIngredient = formData.get('ingredient');
-    ingredients.push(newIngredient)
-    console.log(ingredients)
+    let newIngredient = formData.get('ingredient');
+
+    if(!newIngredient.length) return;
+    
+    setIngredients(prevIngredient => [...prevIngredient, newIngredient])
+
+    setNewIngredient("")
+
   }
 
   return(
     <main>
       <form className="add-ingredient-form" onSubmit={handleSubmit}>
-        <input type="text" placeholder="e.g. ofe egusi" aria-label="add ingredient"
+        <input type="text" 
+        placeholder="e.g. ofe egusi" aria-label="add ingredient"
         name='ingredient'
+        value={newIngredient}
+        onChange={(event) => setNewIngredient(event.target.value)}
         className='input'/>
         <button type="submit">Add ingredient 
         </button>
