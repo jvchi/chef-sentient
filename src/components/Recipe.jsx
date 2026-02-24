da4428fa77e63c0df1d91d7096b9993af3d7d983
jvchi <elechidestinyj@gmail.com>
2026-02-24 20:17:59 +0100

    fixed markdown text formatting

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -3,9 +3,12 @@ import ReactMarkdown from 'https://esm.sh/react-markdown@7'
 
 export default function Recipe(props) {
   return (
-    <ReactMarkdown className='recipe-markdown max-w-[800px] mx-auto' aria-live='polite'>
-      {/* <h2 className='text-[14px] font-medium my-4 sm:text-[18px] px-4 sm:px-2'>Chef Jed Recommends:</h2> */}
-      {props.recipe}
-    </ReactMarkdown>
+    <section className='px-6 sm:px-8  max-w-[800px] mx-auto mb-40 mb-'>
+      <h2 className='text-[14px] font-medium mb-4 sm:text-[18px]'>Chef Jed Recommends:</h2>
+      <ReactMarkdown className='recipe-markdown' aria-live='polite'>
+        {props.recipe}
+      </ReactMarkdown>
+    </section>
+
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,27 +6,27 @@ import ChefLogo from '../assets/chef.svg?react'
 
 gsap.registerPlugin(DrawSVGPlugin)
 
-export default function Navbar(){
+export default function Navbar() {
   const container = useRef()
 
-  useGSAP(()=>{
+  useGSAP(() => {
     gsap.set('.chef-logo path, .chef-logo circle, .chef-logo line', { drawSVG: "0%" });
 
     let tl = gsap.timeline();
     tl.from('.chef-logo', { x: 80, duration: 1, ease: 'power4.out' })
       .to('.chef-logo path, .chef-logo circle, .chef-logo line', {
-        drawSVG: "100%", 
+        drawSVG: "100%",
         duration: 2,
         stagger: 0.1
       })
       .from('.chef-name', { x: -40, duration: .5, ease: 'power4.in', opacity: 0 }, 0.1)
   }, [])
 
-  return(
+  return (
     <section>
-      <nav className="w-full bg-neutral-50 min-h-16 flex items-center justify-center h-16 border-neutral-300 border-[0.5px] select-none">
-        <div ref={container} className="w-max flex flex-row gap-4 items-center text-[clamp(1rem,5vw,2rem)]"> 
-          <ChefLogo className="chef-logo w-[clamp(48px,6vw,50px)] h-[clamp(64px,10vw,50px)]" 
+      <nav className="w-full bg-neutral-100 min-h-16 flex items-center justify-center h-16 border-neutral-300 border-b-[0.5px] select-none">
+        <div ref={container} className="w-max flex flex-row gap-4 items-center text-[clamp(1rem,5vw,2rem)]">
+          <ChefLogo className="chef-logo w-[clamp(48px,6vw,50px)] h-[clamp(64px,10vw,50px)]"
           />
           <span className="chef-name font-thin text-neutral-800">Chef JED</span>
         </div>
